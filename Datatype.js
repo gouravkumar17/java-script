4a4ce793414e8d7e0daf9f5b63baf624df778c9f
Gourav Kumar <gk7410993@gmail.com>
2025-01-17 23:32:08 +0530

    Add files via upload

---
+++ Datatype.js
@@ -0,0 +1,22 @@
+//console.log("Gourav Kumar");
+// Properties of var
+// var a = 10;
+// console.log(a);
+// a = "gourav";
+// console.log(a);
+// var a = 30;
+// console.log(a);
+
+// Properties of let
+let a = 10;
+console.log(a);
+a = "gourav";
+console.log(a);
+// let a = 30;
+// console.log(a);
+
+// Properties of const
+// const a = 10;
+// console.log(a);
+// a = 30;
+// console.log(a);
