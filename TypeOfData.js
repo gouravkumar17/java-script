b839c07c5b1598caad95cb4e2dc469b10fc0d27e
Gourav Kumar <gk7410993@gmail.com>
2025-01-20 10:14:11 +0530

    Add files via upload

---
+++ TypeOfData.js
@@ -0,0 +1,14 @@
+const a = 10.5;
+// console.log(typeof a);
+var b = "10";
+// console.log(typeof b);
+// console.log(a-b);
+// console.log(a-parseInt(b));
+var c = true;
+console.log(typeof c);
+var d;
+console.log(typeof d);
+var d = null;
+console.log(typeof d);
+var t = 5161215121651626184984941n;
+console.log(typeof t);
