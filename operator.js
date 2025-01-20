5bfa8228d94d568ba450224915b06a7c5591fd34
Gourav Kumar <gk7410993@gmail.com>
2025-01-20 14:52:42 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,18 @@
+// var a = -10;
+// console.log(a == 10);
+// console.log(a == "10");
+// console.log(a === "10");
+// console.log(a++);
+// console.log(++a);
+// var a = "10";
+// var b = +a;
+// console.log(+a);
+// console.log(-a);
+// console.log(b+a+5);
+
+// & operator
+console.log(10 & 4);
+console.log(19 & 18);
+
+// or operator
+console.log(10 | 4);
