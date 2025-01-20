91f8993842c1bccb263f063a2e433bd7d6b65527
Gourav Kumar <gk7410993@gmail.com>
2025-01-20 10:13:01 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,29 @@
+// var a = -10;
+// if(!a){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+//switch
+// var a = 30;
+// switch(a){
+//     case 10:
+//         console.log("condition is 10");
+//         break;
+//     case 20:
+//         console.log("condition is 20");
+//         break;
+//     case 30:
+//         console.log("condition is 30");
+//         break;
+//     default:
+//         console.log("welcome");
+// }
+
+//ternary
+var a = 100;
+var b = 20;
+var c = 30;
+console.log((a>b && a>c)?a:(b>c && b>a)?b:(c>a && c>b)?c:a);
