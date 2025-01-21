c622746f01be070120ff7ffe23f978f630c7cbad
Gourav Kumar <gk7410993@gmail.com>
2025-01-21 10:15:28 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,31 @@
+// for(let i=0;i<10;i++){
+//     console.log("Gourav");
+// }
+
+// for(100;100;100){
+//     console.log("Gourav");
+// }
+
+// for(;0;0){
+//     console.log("Gourav");
+// }
+
+// var n = 3;
+// for(let i=0;i<n;i++){
+//     for(let j=i;j<n;j++){
+//         console.log("*");
+//     }
+//     console.log();
+// }
+
+// var a = 10;
+// while(a){
+//     console.log("Gourav");
+//     a--;
+// }
+
+var a = 5;
+do{
+    console.log("hello");
+    a--;
+}while(a);
