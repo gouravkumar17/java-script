05e4c7def3cd596cb980268aa6be52032055948d
Gourav Kumar <gk7410993@gmail.com>
2025-01-24 10:44:17 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,21 @@
+// function name(){
+//     console.log("Gourav");
+// }
+// name();
+
+
+function add(x,y){
+    console.log(x+y);
+    return "welcome"
+}
+console.log(add(4,5));
+
+//1)function statement
+//2)function expression
+//3)fat arrow function
+//4)anonymous function
+//5)callback function
+//6)higher order function
+
+// Imp question : diff between funct. statement and funct. expression
+//Global execution context is created by the js engine even before the execution of the script 
