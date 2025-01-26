5bc67ef3b3a11733306a1f283a5ecd009722823a
Gourav Kumar <gk7410993@gmail.com>
2025-01-26 20:10:45 +0530

    Add files via upload

---
+++ hoisting.js
@@ -0,0 +1,15 @@
+// console.log("Gourav Kumar");
+// console.log(a);
+// var a = 10;
+// console.log(a);
+
+
+{
+    console.log(a);//error
+    let a = 10;
+    console.log(a);
+}
+
+//var is hoisted properly
+//let is hoisted improperly in part of js engine called temporal deadzone(Proved)
+//const is not hoisted properly
