7df9fbb635a257f474d9002c9704d745b45bce0c
Gourav Kumar <gk7410993@gmail.com>
2025-01-28 21:30:29 +0530

    Add files via upload

---
+++ Array/arr2.js
@@ -0,0 +1,43 @@
+//find   return item
+// var arr = [1,2,3,4,5];
+// var x = arr.find((i,idx)=>{
+//     return i>39;
+// })
+// console.log(x);
+
+
+//find index   return index
+// var arr = [1,2,3,4,5];
+// var x = arr.findIndex((i,idx)=>{
+//     return i>3;
+// })
+// console.log(x);
+
+
+//Some  return boolean
+// var arr = [1,2,3,4,5,6]
+// var x = arr.some((i,idx)=>{
+//     return i==3
+// })
+// console.log(x);
+
+//every
+// var arr = [1,2,3,4,5,6]
+// var x = arr.every((i,idx)=>{
+//     return i>5;
+// })
+// console.log(x);
+
+// var arr = [1,2,[3,4,5,["a",[[[[[[[[[[[["Gourav"]]]]]]]]]]]],"b","c"],5,6],"Gourav"];
+// var arr1 = arr.flat(Infinity);
+// console.log(arr1);
+
+//contactination
+// var arr1 = [1,2,3];
+// var arr2 = [4,5,6];
+// var arr3 = [7,8,9];
+// var arr4 = arr1.concat(arr2,arr3);
+// console.log(arr4);
+// var arr5 = arr1.concat(arr2,arr3,arr4);
+// console.log(arr5);
+
