8ce3a196dfbc8bcba3e2fcf96a04fdfe45a0f680
Gourav Kumar <gk7410993@gmail.com>
2025-01-30 22:00:15 +0530

    Add files via upload

---
+++ Array/distructure.js
@@ -0,0 +1,14 @@
+// var [a,b,c] = [1,2,3,4,5];
+// console.log(a);
+
+// var arr = [1,2,3,4,5];
+// var [a,b] = arr;
+// console.log(a,b);
+
+//join arr=string
+//split string=arr
+var arr = ["G","o","u","r","a","v"]
+var str = arr.join();
+var arr1 = str.split(",");
+console.log(arr1);
+console.log(arr1.reverse())
